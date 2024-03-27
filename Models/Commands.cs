b52da8f88c64b645a57dc0df74c23e7544cf07fe
yassineboucham <127396067+yassineboucham@users.noreply.github.com>
2024-03-27 08:45:36 +0000

    create the model and in commands.sc create a class command

--- Models/Commands.cs
+++ Models/Commands.cs
@@ -1,4 +1,10 @@
-namespace crud-in-.net.Models
+namespace crud.Models
 {
-
+    public class Command
+    {
+        public int id { get; set; }
+        public string? HowTo { get; set; }
+        public string? Line { get; set; }
+        public string? Platform { get; set; }
+    }
 }
